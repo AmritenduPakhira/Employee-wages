633d5a77e9971b6b3f504cd0528276ca0435e26f
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-17 22:46:49 +0530

    [Amritendu] new constructor add

--- dailyEmployeeWage.js
+++ dailyEmployeeWage.js
@@ -1,74 +1,31 @@
-const readline = require('readline');
+class Employeewagecalculator{
 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-class DailyEmployeeWages {
-    constructor(name) {
-        this.name = name;
-    }
-
-    checkAttendance() {
-
-        const attendance = Math.floor(Math.random() * 2);
-
-        if (attendance === 1) {
-            console.log(`${this.name} is Present`);
-            this.askWorkType();
-        } else {
-            console.log(`${this.name} is Absent`);
-            this.askNextEmployee();
-        }
-    }
-
-    askWorkType() {
-        rl.question("Enter work type (Full-Time/Part-Time): ", (workType) => {
-            const wagePerHour = 20;
-            let hoursWorked;
-
-            if (workType.toLowerCase() === "full-time") {
-                hoursWorked = 8;
-                console.log(`${this.name} worked Full-Time (8 hours).`);
-            } else if (workType.toLowerCase() === "part-time") {
-                hoursWorked = 4;
-                console.log(`${this.name} worked Part-Time (4 hours).`);
-            } else {
-                console.log("Invalid work type.");
-                rl.close();
-                return;
-            }
-
-            const totalWage = hoursWorked * wagePerHour;
-            console.log(`Total Wage for ${this.name}: $${totalWage}`);
-
-            this.askNextEmployee();
-        });
-    }
-
-    askNextEmployee() {
-        rl.question("Do you want to check another employee? (y/n): ", (answer) => {
-            if (answer.toLowerCase() === 'y') {
-                askEmployee();
-            } else {
-                rl.close();
-            }
-        });
+    constructor(wageperhour = 20){
+        this.wageperhour = wageperhour;
+        this.fulltimehours = 8;
+        this.parttimehours = 4;
     }
 }
 
+const ispresent = Math.floor(Math.random() * 2);
 
-function askEmployee() {
-    rl.question("Enter employee name (or type 'n' to stop): ", function (employeeName) {
-        if (employeeName.toLowerCase() === 'n') {
-            rl.close();
-        } else {
-            const employee = new DailyEmployeeWages(employeeName); // Create an object of DailyEmployeeWages
-            employee.checkAttendance();
-        }
-    });
+if(ispresent == 0){
+    console.log("Employee is absent");
+    console.log("Daily wage is 0");
+}else{ 
+    console.log("Employee is present");
+    const check = Math.floor(Math.random() * 2)+1;
+   
+      if(check == 1){
+        console.log("Employee is full time");
+        const emp = new Employeewagecalculator();
+        const dailywage = emp.wageperhour * emp.fulltimehours;
+        console.log("Daily wage is " + dailywage);
+      }else{
+        console.log("Employee is part time");
+        const emp = new Employeewagecalculator();
+        const dailywage = emp.wageperhour * emp.parttimehours;
+        console.log("Daily wage is " + dailywage);
+      }
+
 }
-
-askEmployee();
