9052048dbdaee0977ca742bd135b70dfa0c192ad
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-18 10:56:32 +0530

    [Amritendu] and file class  name chnage

--- dailyEmployeeWage.js
+++ dailyEmployeeWage.js
@@ -1,5 +1,5 @@
 
-class Emmployeewages {
+class Emmployee {
     constructor(wageperhour = 20) {
         this.wageperhour = wageperhour;
         this.workingdays = 20;
@@ -26,20 +26,21 @@ class Emmployeewages {
             this.totalwalkingdays++;
 
             if (check === 1) {
-                console.log("Employee is present " + this.totalwalkingdays + " day");
+                console.log("Employee is present: " + this.totalwalkingdays + " day");
                 const checker = Math.floor(Math.random() * 2) + 1; 
     
                 const hours = this.getworkhours(checker);
     
                 if (hours === 8) {
-                    console.log("Employee is full time present");
+                    console.log("Employee is Present : Full time");
                 } else if (hours === 4) {
-                    console.log("Employee is part time present");
+                    console.log("Employee is Present : Part time");
                 }
 
                 
                 const empwage = hours * this.wageperhour;
                 console.log("Employee wage is: " + empwage);
+                console.log(`Total hours worked : ${hours} hours in a day`);
                 
                 this.totalworkinghours += hours;
             
@@ -47,14 +48,17 @@ class Emmployeewages {
                
             } else {
 
-                console.log("Employee is absent" + this.totalwalkingdays + " day");
+                console.log("Employee is Absent : " + this.totalwalkingdays + " day");
                 console.log("Employee wage is: 0");
+                console.log(`Total hours worked : ${this.totalworkinghours} hours in a day`);
                 continue;
             }
 
 
 
         }
+
+        console.log(`\nSummary of employee wage:`);
 
         console.log("Total working hours: " + this.totalworkinghours);
         console.log("Total wage: " + this.totalwage);
@@ -64,5 +68,5 @@ class Emmployeewages {
     }
 }
 
-const result = new Emmployeewages();
+const result = new Emmployee();
 result.calculatewage();
