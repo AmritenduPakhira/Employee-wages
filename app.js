8c75b64f573fb7bd40908ace66b12ffe11992842
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-18 11:12:42 +0530

    [Amritendu] class name change and redefine the code and midfied app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 class Emmployee {
-    constructor(wageperhour = 20) {
+    constructor(wageperhour = 20, workingdays = 20, maxHours = 160) {
         this.wageperhour = wageperhour;
-        this.workingdays = 20;
+        this.workingdays = workingdays;
+        this.maxHours = maxHours;
         this.totalwage = 0;
         this.totalworkinghours = 0;
         this.totalwalkingdays = 0;
@@ -20,51 +20,41 @@ class Emmployee {
     }
 
     calculatewage() {
-        const check = Math.floor(Math.random() * 2); 
-
-        while(this.totalwalkingdays  < this.workingdays) {
+        while (this.totalwalkingdays < this.workingdays && this.totalworkinghours < this.maxHours) {
             this.totalwalkingdays++;
+
+            const check = Math.floor(Math.random() * 2); 
 
             if (check === 1) {
                 console.log("Employee is present: " + this.totalwalkingdays + " day");
                 const checker = Math.floor(Math.random() * 2) + 1; 
-    
                 const hours = this.getworkhours(checker);
-    
+
                 if (hours === 8) {
                     console.log("Employee is Present : Full time");
                 } else if (hours === 4) {
                     console.log("Employee is Present : Part time");
                 }
 
-                
                 const empwage = hours * this.wageperhour;
                 console.log("Employee wage is: " + empwage);
-                console.log(`Total hours worked : ${hours} hours in a day`);
-                
-                this.totalworkinghours += hours;
-            
-                this.totalwage += empwage;
-               
-            } else {
+                console.log(`Total hours worked today: ${hours} hours`);
 
+                this.totalworkinghours += hours;
+                this.totalwage += empwage;
+            } else {
                 console.log("Employee is Absent : " + this.totalwalkingdays + " day");
                 console.log("Employee wage is: 0");
-                console.log(`Total hours worked : ${this.totalworkinghours} hours in a day`);
-                continue;
+                console.log(`Total hours worked today: 0 hours`);
             }
 
-
-
+            console.log("--------------------------------");
         }
 
-        console.log(`\nSummary of employee wage:`);
-
+        console.log(`\nFinal Summary:`);
         console.log("Total working hours: " + this.totalworkinghours);
         console.log("Total wage: " + this.totalwage);
-        console.log("Total working days: " + this.totalwalkingdays)
-
-        
+        console.log("Total working days: " + this.totalwalkingdays);
     }
 }
 
