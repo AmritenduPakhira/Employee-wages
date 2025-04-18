42bacf72566886afbd2a3cebd2bee3dcdb86c8b5
AmritenduPakhira <pakhiraamritendu@gmail.com>
2025-04-18 11:00:43 +0530

    [Amritendu] class name change and redefine the code

--- app.js
+++ app.js
@@ -1,44 +1,72 @@
-// Importing readline module
-const readline = require('readline');
 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-
-class EmployeeAttendanceChecker {
-    constructor(name) {
-        this.name = name;
+class Emmployee {
+    constructor(wageperhour = 20) {
+        this.wageperhour = wageperhour;
+        this.workingdays = 20;
+        this.totalwage = 0;
+        this.totalworkinghours = 0;
+        this.totalwalkingdays = 0;
     }
 
-    checkAttendance() {
-
-        const attendance = Math.floor(Math.random() * 2);
-
-        if (attendance === 1) {
-            console.log(`${this.name} is Present`);
-        } else {
-            console.log(`${this.name} is Absent`);
+    getworkhours(empcheck) {
+        switch (empcheck) {
+            case 1:
+                return 8;
+            case 2:
+                return 4;
+            default:
+                return 0;
         }
+    }
+
+    calculatewage() {
+        const check = Math.floor(Math.random() * 2); 
+
+        while(this.totalwalkingdays  < this.workingdays) {
+            this.totalwalkingdays++;
+
+            if (check === 1) {
+                console.log("Employee is present: " + this.totalwalkingdays + " day");
+                const checker = Math.floor(Math.random() * 2) + 1; 
+    
+                const hours = this.getworkhours(checker);
+    
+                if (hours === 8) {
+                    console.log("Employee is Present : Full time");
+                } else if (hours === 4) {
+                    console.log("Employee is Present : Part time");
+                }
+
+                
+                const empwage = hours * this.wageperhour;
+                console.log("Employee wage is: " + empwage);
+                console.log(`Total hours worked : ${hours} hours in a day`);
+                
+                this.totalworkinghours += hours;
+            
+                this.totalwage += empwage;
+               
+            } else {
+
+                console.log("Employee is Absent : " + this.totalwalkingdays + " day");
+                console.log("Employee wage is: 0");
+                console.log(`Total hours worked : ${this.totalworkinghours} hours in a day`);
+                continue;
+            }
+
+
+
+        }
+
+        console.log(`\nSummary of employee wage:`);
+
+        console.log("Total working hours: " + this.totalworkinghours);
+        console.log("Total wage: " + this.totalwage);
+        console.log("Total working days: " + this.totalwalkingdays)
+
+        
     }
 }
 
-function askEmployee() {
-    rl.question("Enter employee name(or type 'n' to stop): ", function (employeeName) {
-        if (employeeName.toLowerCase() === 'n') {
-
-            rl.close();
-
-        } else {
-            const employee = new EmployeeAttendanceChecker(employeeName);
-            employee.checkAttendance();
-            askEmployee(); 
-
-        }
-    });
-
-}
-
-askEmployee();
+const result = new Emmployee();
+result.calculatewage();
